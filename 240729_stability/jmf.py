5bd515d07fc232551aa557b1e0b18baeff6847fd
Jean-Michel Friedt <friedtj@free.fr>
2024-08-04 09:58:32 +0200

    add frequency offset

--- 240729_stability/jmf.py
+++ 240729_stability/jmf.py
@@ -7,6 +7,6 @@ m=0
 while True:
     data, addr=sock.recvfrom(1500)
     foo.ParseFromString(data)
-    print(f"{m}: TOW={foo.tow_at_current_symbol_ms} dt={foo.user_clk_offset} x={foo.pos_x} y={foo.pos_y} z={foo.pos_z}")
+    print(f"{m}: TOW={foo.tow_at_current_symbol_ms} dt={foo.user_clk_offset} x={foo.pos_x} y={foo.pos_y} z={foo.pos_z} df={foo.user_clk_drift_ppm}")
     m=m+1
 
